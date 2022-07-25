28780a3ff25f858dbdc352f41e32b58cd072bafe
Michtorte <hirota3@hera.eonet.ne.jp>
2022-07-25 16:02:53 +0000

    roll 関数を実装

--- src/dicebot/math/multidice.py
+++ src/dicebot/math/multidice.py
@@ -1,6 +1,15 @@
+from random import randrange
+
+
 def roll(face: int, num: int) -> int:
-    raise NotImplementedError()
+    multiroll = 0
+
+    for _ in range(num):
+        singleroll = randrange(1, face + 1)
+        multiroll += singleroll
+
+    return multiroll
 
 
 def roll_by_str(rep: str) -> int:
-    raise NotImplementedError()
+    raise NotImplementedError
