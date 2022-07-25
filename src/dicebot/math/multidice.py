c1b113dccf903f97fc515ce1aede669e1d89e927
Michtorte <hirota3@hera.eonet.ne.jp>
2022-07-25 18:41:28 +0000

    work in progress

--- src/dicebot/math/multidice.py
+++ src/dicebot/math/multidice.py
@@ -1,9 +1,17 @@
 from random import randrange
+from re import fullmatch
 
 
-def roll(face: int, num: int) -> int:
+def roll(num: int, face: int) -> int:
     return sum(randrange(1, face + 1) for _ in range(num))
 
 
-def roll_by_str(rep: str) -> int:
-    raise NotImplementedError
+def roll_by_str(expr: str) -> int:
+    match = fullmatch(r"(?P<num>[0-9]+)d(?P<face>[0-9]+)", expr)
+
+    if match:
+        num = int(match.group("num"))
+        face = int(match.group("face"))
+        return roll(num, face)
+    else:
+        raise ValueError(expr)
--- tests/test_math/test_multidice.py
+++ tests/test_math/test_multidice.py
@@ -6,21 +6,25 @@ from dicebot.math.multidice import roll, roll_by_str
 
 
 @pytest.mark.parametrize(
-    ("face", "num", "condition"),
+    ("num", "face", "condition"),
     [
         (1, 1, lambda res: 1 <= res <= 1),
-        (2, 1, lambda res: 1 <= res <= 2),
+        (31, 1, lambda res: 31 <= res <= 31),
+        (1, 2, lambda res: 1 <= res <= 2),
+        (10, 20, lambda res: 10 <= res <= 200),
     ],
 )
-def test_roll(face: int, num: int, condition: Callable[[int], bool]):
-    assert condition(roll(face, num))
+def test_roll(num: int, face: int, condition: Callable[[int], bool]):
+    assert condition(roll(num, face))
 
 
 @pytest.mark.parametrize(
     ("rep", "condition"),
     [
         ("1d1", lambda res: 1 <= res <= 1),
-        ("2d1", lambda res: 1 <= res <= 2),
+        ("1d2", lambda res: 1 <= res <= 2),
+        ("25d1", lambda res: 25 <= res <= 25),
+        ("20d10", lambda res: 20 <= res <= 200),
     ],
 )
 def test_roll_by_str(rep: str, condition: Callable[[int], bool]):
