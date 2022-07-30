ea4f77331947fce9e0880ee84ff1e392f6be3595
lambda-chat <chat.algebrique@gmail.com>
2022-07-30 14:55:05 +0000

    [wip] update default cdk files

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/cdk-stack';
+import { BridgeLambdaStack } from '../lib/bridge-lambda-stack';
+import { EngineLambdaStack } from '../lib/engine-lambda-stack';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack');
+new BridgeLambdaStack(app, 'BridgeLambdaStack');
+new EngineLambdaStack(app, 'EngineLambdaStack');
--- cdk/test/cdk.test.ts
+++ cdk/test/cdk.test.ts
@@ -1,17 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Template, Match } from 'aws-cdk-lib/assertions';
-import * as Cdk from '../lib/cdk-stack';
+import { BridgeLambdaStack } from '../lib/bridge-lambda-stack';
 
-test('SQS Queue and SNS Topic Created', () => {
-  const app = new cdk.App();
-  // WHEN
-  const stack = new Cdk.CdkStack(app, 'MyTestStack');
-  // THEN
+// test('SQS Queue and SNS Topic Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new BridgeLambdaStack(app, 'MyTestStack');
+//   // THEN
 
-  const template = Template.fromStack(stack);
+//   const template = Template.fromStack(stack);
 
-  template.hasResourceProperties('AWS::SQS::Queue', {
-    VisibilityTimeout: 300
-  });
-  template.resourceCountIs('AWS::SNS::Topic', 1);
-});
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+//   template.resourceCountIs('AWS::SNS::Topic', 1);
+// });
