7c186a2444b214e5f54d59d305f61100a98fae2e
lambda-chat <chat.algebrique@gmail.com>
2022-08-04 02:56:53 +0900

    make lambda/app.py to pure api function

--- lambda/app.py
+++ lambda/app.py
@@ -1,24 +1,102 @@
+from dataclasses import dataclass
+from enum import Enum
 import os
-from typing import Any, cast
+from typing import Any, Optional
 
 from aws_lambda_powertools.logging import Logger
 
-from dicebot.multidice import roll_by_expr
+from nacl.signing import VerifyKey
 
 logger = Logger(service="dicebot-engine")
 
 
+MAX_JSON_DEPTH = 3
+MAX_LOGGING_MESSAGE_LENGTH = 1000
+
+
+@dataclass
+class Response:
+    status_code: int
+    body: Any = None
+
+    def to_json(self) -> dict[str, Any]:
+        if self.body is None:
+            return {"statusCode": self.status_code}
+        return {"statusCode": self.status_code, "body": str(self.body)}
+
+
+class DiscordMessageType(Enum):
+    Ping = 1
+    AnswerWithInvocation = 4
+
+
+@dataclass
+class DiscordResponse:
+    type: DiscordMessageType
+    body: Optional[str] = None
+    content: Optional[str] = None
+
+    def to_json(self) -> dict[str, Any]:
+        if self.type is DiscordMessageType.Ping:
+            return Response(200, str({"type": DiscordMessageType.Ping})).to_json()
+        if self.type is DiscordMessageType.AnswerWithInvocation:
+            return {
+                "type": self.type,
+                "data": {
+                    "content": self.content,
+                },
+            }
+        raise ValueError(self)
+
+
+def sanitize_json(json_like: Any, depth: int = 0) -> Any:
+    ty = type(json_like)
+    if ty is dict:
+        if depth <= MAX_JSON_DEPTH:
+            return {k: sanitize_json(v, depth=depth + 1) for k, v in json_like.items()}
+        else:
+            return f"<dict of depth > {MAX_JSON_DEPTH}>"
+    elif ty is list:
+        return f"<list of length {len(json_like)}>"
+    elif ty in (None, int, float, str):
+        return json_like
+    else:
+        raise ValueError(json_like)
+
+
 @logger.inject_lambda_context
-def handler(event, context) -> dict[str, Any]:  # NOQA
-    if event.get("api_key", "") != os.environ["LAMBDA_API_KEY"]:
-        return {"statusCode": 401}  # Not Authorized
+def handler(event: dict, context) -> dict[str, Any]:  # NOQA
+    event = sanitize_json(event)
+    logger.info(str(event)[:MAX_LOGGING_MESSAGE_LENGTH])
 
-    if (expr := event.get("expression")) is None:
-        return {"statusCode": 400}  # No expression
-    expr = cast(str, expr)
+    public_key: str = os.environ["PUBLIC_KEY"]
+    signature: str = event["headers"]["x-signature-ed25519"]
+    timestamp: str = event["headers"]["x-signature-timestamp"]
+    body: str = event["body"]
 
-    try:
-        value = roll_by_expr(expr)  # FIXME: 一般の expression に対して実行できるようにする
-        return {"statusCode": 200, "body": str(value)}  # OK
-    except:  # NOQA
-        return {"statusCode": 400, "body": f"invalid expression `{expr}`"}  # Invalid
+    verify_key = VerifyKey(
+        key=public_key.encode("hex"),
+    )
+
+    is_verified = verify_key.verify(
+        smessage=(timestamp + body).encode("hex"),
+        signature=signature.encode("hex"),
+    )
+    if not is_verified:
+        return Response(401, "invalid request signature").to_json()
+
+    if data := event.get("data"):
+        if command_name := data.get("name"):  # handle /foo command etc.
+            logger.info(f"command_name: {command_name}")
+            try:
+                return DiscordResponse(
+                    type=DiscordMessageType.AnswerWithInvocation,
+                    content="<result>",  # FIXME
+                ).to_json()
+            except:
+                return Response(
+                    status_code=401,
+                    body=f"command {command_name} does not accept <dice-format>.",  # FIXME  # NOQA
+                ).to_json()
+
+    return Response(status_code=401, body="invalid command").to_json()
